d5c5d0a82c76fb1c45c0fd976a0e800e3e5da992
Joao Giordani Donasolo <jpdonasolo@gmail.com>
2025-11-13 13:41:27 +0100

    Encapsulate script into a main function

--- src/download_datasets.py
+++ src/download_datasets.py
@@ -3,26 +3,46 @@ import requests
 import os
 import urllib3
 import subprocess
+import argparse
+
 
 if not os.path.exists("rar/unrar"):
     raise FileNotFoundError("`unrar` executable not found. Download and unpack it from https://www.win-rar.com/download.html")
 
-# For requests download without SSL certificate verification
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# https://www.kaggle.com/datasets/hasyimabdillah/workoutfitness-video
-print("Downloading dataset: hasyimabdillah/workoutfitness-video")
-path = kagglehub.dataset_download("hasyimabdillah/workoutfitness-video")
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
-# https://www.kaggle.com/datasets/mohamadashrafsalama/pushup
-print("Downloading dataset: mohamadashrafsalama/pushup")
-path = kagglehub.dataset_download("mohamadashrafsalama/pushup")
 
-# https://www.crcv.ucf.edu/data/UCF101.php
-def download_ucf101(force_download: str = False, outdir: str = "datasets"):
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--abs-path-outdir", type=str, default=os.path.join(ROOT_DIR, "datasets"))
+    parser.add_argument("--force-download", action="store_true")
+    
+    return parser.parse_args()
+
+
+def main(
+    abs_path_outdir: str,
+    force_download: bool,
+):
+    os.makedirs(abs_path_outdir, exist_ok=True)
+
+    # For requests download without SSL certificate verification
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    # https://www.kaggle.com/datasets/hasyimabdillah/workoutfitness-video
+    print("Downloading dataset: hasyimabdillah/workoutfitness-video")
+    path = kagglehub.dataset_download("hasyimabdillah/workoutfitness-video")
+
+    # https://www.kaggle.com/datasets/mohamadashrafsalama/pushup
+    print("Downloading dataset: mohamadashrafsalama/pushup")
+    path = kagglehub.dataset_download("mohamadashrafsalama/pushup")
+
+
+    # https://www.crcv.ucf.edu/data/UCF101.php
     print("Downloading dataset: UCF101")
     url = "https://www.crcv.ucf.edu/data/UCF101/UCF101.rar"
-    local_path = os.path.join(outdir, "UCF101.rar")
+    local_path = os.path.join(abs_path_outdir, "UCF101.rar")
     
     if not os.path.exists(local_path) or force_download:
         with requests.get(url, verify=False, stream=True) as response:
@@ -44,10 +64,11 @@ def download_ucf101(force_download: str = False, outdir: str = "datasets"):
         print("Dataset UCF101 already exists. Skipping download")
 
     print("Extracting dataset: UCF101")
-    HERE = os.path.dirname(os.path.abspath(__file__))
-    if os.path.exists(os.path.join(outdir, "UCF-101")):
+    if os.path.exists(os.path.join(abs_path_outdir, "UCF-101")):
         print("Dataset UCF101 already extracted. Skipping extraction")
         return
-    subprocess.run([f"{HERE}/rar/unrar",  "x", local_path, outdir], check=True)
+    subprocess.run([f"{ROOT_DIR}/rar/unrar",  "x", local_path, abs_path_outdir], check=True)
 
-download_ucf101(force_download=False, outdir="datasets")
+if __name__ == "__main__":
+    args = parse_args()
+    main(**args.__dict__)
