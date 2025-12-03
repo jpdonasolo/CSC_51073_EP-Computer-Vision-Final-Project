61a0c4305fbaabfb2dcf55d780f860a364448272
Joao Donasolo <jpdonasolo@gmail.com>
2025-12-03 18:05:28 +0100

    Docs

--- src/workout_tracker/recorder.py
+++ src/workout_tracker/recorder.py
@@ -1,3 +1,8 @@
+"""
+Class used to record prediction probabilities to a log file
+"""
+
+
 import csv
 from pathlib import Path
 
@@ -6,7 +11,9 @@ import constants as c
 
 
 class Recorder:
-
+    """
+    Records prediction probabilities to file in the __init__
+    """
     def __init__(self, output: str = None):
         if output is None:
             self._is_recording = False
@@ -17,17 +24,23 @@ class Recorder:
 
         self.output.parent.mkdir(parents=True, exist_ok=True)
         self.output.unlink(missing_ok=True)
-            
+        
+        # Add header
+        header = ["timestamp"] + ["prob_" + label for label in sorted(c.LABEL_TO_COUNT.keys())]
         with open(self.output, "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f, delimiter=";")
-            writer.writerow(["timestamp"] + ["prob_" + label for label in sorted(c.LABEL_TO_COUNT.keys())])
+            writer.writerow(header)
 
 
     def record(self, info: dict, sep=";"):
+        """
+        Records the prediction probabilities to the csv file.
+        """
         if not self._is_recording:
             return
 
         timestamp = info.pop("timestamp")
+        # Make sure numbers will be in the correct order
         assert sorted(info.keys()) == sorted(c.LABEL_TO_COUNT.keys())
         info = [info[k] for k in sorted(info.keys())]
 
