ab55531fb90d9ba3b3bd543d9506f5f5c202450f
Joao Donasolo <jpdonasolo@gmail.com>
2025-12-07 18:49:36 +0100

    Code for running on all videos

--- src/benchmark/manual_inspection.py
+++ src/benchmark/manual_inspection.py
@@ -10,11 +10,7 @@ sys.path.append(str(PROJECT_ROOT / "src"))
 sys.path.append(str(PROJECT_ROOT / "src/workout_tracker"))
 
 from workout_tracker.workout_model import WorkoutModel
-import workout_tracker.constants as c
 
-
-
-VIDEO_ID = 6
 VIDEO_TO_FPS = {
     1: 29.72,
     2: 29.94,
@@ -24,68 +20,74 @@ VIDEO_TO_FPS = {
     6: 18.56,
 }
 
+BUFFER_SIZE_SECONDS = 3
+PREDICTION_INTERVAL = 0.2
+WARMUP_TIME = 2.0
+UP_CONFIDENCE_THRESHOLD = 0.95
+ALPHA = 0.8
+TIMEOUT = 0.5
+NUM_FRAMES = 8
+TEMPERATURE = 0.9
+
 
 def main():
-    model = WorkoutModel(
-            "timesformer",
-            # ckpt_path=PROJECT_ROOT / "checkpoints" / "timesformer_best.pt",
-            output=PROJECT_ROOT / "src" / "benchmark" / "manual_inspection" / "results" / f"pretrained_{VIDEO_ID}.csv",
-            temperature=0.9,
-            num_frames=8,
-            timeout=0.5,
-        )
+
+
+    for video_id in range(1, 7):
+        print(f"Processing video {video_id}")
+
+        model = WorkoutModel(
+                "timesformer",
+                ckpt_path=PROJECT_ROOT / "checkpoints" / "timesformer_best.pt",
+                output=PROJECT_ROOT / "src" / "benchmark" / "manual_inspection" / "results" / f"finetuned_{video_id}.csv",
+                temperature=TEMPERATURE,
+                num_frames=NUM_FRAMES,
+                timeout=TIMEOUT,
+                up_confidence=UP_CONFIDENCE_THRESHOLD,
+                alpha=ALPHA,
+            )
     
-    # Load video from "src/benchmark/manual_inspection"
-    video_path = PROJECT_ROOT / "src" / "benchmark" / "manual_inspection" / "videos" / f"{VIDEO_ID}.mp4"
-    
-    # Option 1: Load all frames as numpy arrays
-    cap = cv2.VideoCapture(str(video_path))
-    frames = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frames.append(frame)
-
-    cap.release()
-
-    # Resize frames to 640x480
-    # frames = [cv2.resize(frame, (640, 480)) for frame in frames]
-
-    print(f"Loaded {len(frames)} frames")
-    print(f"First frame shape: {frames[0].shape}")  # (H, W, 3)
-
-
-    camera_framerate = VIDEO_TO_FPS[VIDEO_ID]
-    buffer_size = int(c.BUFFER_SIZE_SECONDS * camera_framerate)
-    
-    last_infer_time = time.time()
-    cur_frame = c.WARMUP_TIME * camera_framerate
-
-
-    while True:
-        current_time = time.time()
+        # Load video from "src/benchmark/manual_inspection"
+        video_path = PROJECT_ROOT / "src" / "benchmark" / "manual_inspection" / "videos" / f"{video_id}.mp4"
         
-        if current_time - last_infer_time >= c.PREDICTION_INTERVAL:
-            elapsed_time = current_time - last_infer_time
-            elapsed_frames = elapsed_time * camera_framerate
-            cur_frame = cur_frame + elapsed_frames
-            print(f"Starting inference at {cur_frame / camera_framerate:.2f} seconds")
-            
-            if cur_frame >= len(frames):
+        # Option 1: Load all frames as numpy arrays
+        cap = cv2.VideoCapture(str(video_path))
+        frames = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
                 break
+            frames.append(frame)
 
-            model.predict(frames[int(cur_frame-buffer_size):int(cur_frame)])
-            last_infer_time = current_time
+        cap.release()
+
+        camera_framerate = VIDEO_TO_FPS[video_id]
+        buffer_size = int(BUFFER_SIZE_SECONDS * camera_framerate)
         
-        else:
-            time.sleep(0.01)
-
-    # Wait for the model to finish predicting
-    time.sleep(.5)
-    print("Done")
+        last_infer_time = time.time()
+        cur_frame = WARMUP_TIME * camera_framerate
 
 
+        while True:
+            current_time = time.time()
+            
+            if current_time - last_infer_time >= PREDICTION_INTERVAL:
+                elapsed_time = current_time - last_infer_time
+                elapsed_frames = elapsed_time * camera_framerate
+                cur_frame = cur_frame + elapsed_frames
+                print(f"Starting inference at {cur_frame / camera_framerate:.2f} seconds")
+                
+                if cur_frame >= len(frames):
+                    break
+
+                model.predict(frames[int(cur_frame-buffer_size):int(cur_frame)])
+                last_infer_time = current_time
+            
+            else:
+                time.sleep(0.01)
+
+        # Wait for the model to finish predicting
+        time.sleep(.5)
 
 if __name__ == "__main__":
     main()
